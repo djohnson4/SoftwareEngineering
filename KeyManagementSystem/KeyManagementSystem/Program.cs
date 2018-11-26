58b3b5556eae1e43da1a9647c0e4430715d5a167
Aubrey Bryant <aubbryant@augusta.edu>
2018-11-25 22:42:57 -0500

    populated the database and started on the connection

--- KeyManagementSystem/KeyManagementSystem/Program.cs
+++ KeyManagementSystem/KeyManagementSystem/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace KeyManagementSystem
 {
@@ -17,6 +18,27 @@ namespace KeyManagementSystem
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new LoginForm());
+            SqlConnection connection = new SqlConnection("Database1.mdf");
+            //SqlCommand command = new SqlCommand(user1, connection);
+            //connection.Open();
+            //command.ExecuteNonQuery();
+            //connection.Close();
+                    //SqlDataReader reader = command.ExecuteReader();
+                    //try
+                    //{
+                    //    while (reader.Read())
+                    //    {
+                    //        Console.WriteLine(String.Format("{0}, {1}",
+                    //            reader[0], reader[1]));
+                    //    }
+                    //}
+                    //finally
+                    //{
+                    //    // Always call Close when done reading.
+                    //    reader.Close();
+                    //}
+                }
+            }
         }
-    }
-}
+    
+
