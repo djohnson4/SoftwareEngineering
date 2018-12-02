8b0074e56a376d10211deacb6a2df737a0489d43
jmstubble22 <jmstubble22@gmail.com>
2018-12-02 12:15:57 -0500

    .suo

---
+++ KeyManagementSystem/KeyManagementSystem/Controller/LogoutController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KeyManagementSystem.Boundary;
+
+namespace KeyManagementSystem.Controller
+{
+    class LogoutController
+    {
+        public void logout(int userID)
+        {
+            LoginForm lf = new LoginForm();
+            lf.Show();
+        }
+    }
+}
